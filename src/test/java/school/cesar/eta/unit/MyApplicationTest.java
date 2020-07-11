71b42f0fee130c1d232ad92e7c1c1b9be1badb94
Milton Castro <milton.castro@inovant.com.br>
2020-07-11 15:52:59 -0300

    Add MyAplication stub tests

--- src/test/java/school/cesar/eta/unit/MyApplicationTest.java
+++ src/test/java/school/cesar/eta/unit/MyApplicationTest.java
@@ -1,15 +1,52 @@
 package school.cesar.eta.unit;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MyApplicationTest {
 
+    private MyApplication application;
+
+    @BeforeEach
+    public void testSetup(){
+        application = new MyApplication();
+    }
+
+    @AfterEach
+    public void testTeardown(){
+        application.setService(null);
+    }
+
     @Test
     public void processMessage_returnsTrue() {
         MessageService service = new MessageServiceMocked();
-        MyApplication application = new MyApplication();
         application.setService(service);
         Assertions.assertTrue(application.processMessage("", ""));
+    }
+
+    @Test
+    public void processMessage_returnsFalseWithStub(){
+        MessageService service = new MessageService(){
+            @Override
+            public boolean sendMessage(String msg, String rec){
+                return false;
+            }
+        };
+        application.setService(service);
+        Assertions.assertFalse(application.processMessage(null, null));
+    }
+
+    @Test
+    public void processMessage_returnsTrueWithStub(){
+        MessageService service = new MessageService(){
+            @Override
+            public boolean sendMessage(String msg, String rec) {
+                return true;
+            }
+        };
+        application.setService(service);
+        Assertions.assertTrue(application.processMessage("",null));
     }
 }
